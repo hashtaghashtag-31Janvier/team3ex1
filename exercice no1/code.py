ab15d8d4a15e0001c415dafd5db1fb1028aed768
Frédéric Bolduc <bolducfrederic@gmail.com>
2014-01-31 22:05:37 -0500

    bon ben c'est ça

--- exercice no1/code.py
+++ exercice no1/code.py
@@ -1,17 +1,38 @@
 # -*- coding: utf-8 -*-
 import random
 import itertools
+import time
+
+# Clear l'écran
+import os
+os.system('cls' if os.name=='nt' else 'clear')
 
 # on construit l'alphabet pour le tirage
 consonnes = "bcdfghjklmnpqrstvwxz"
 voyelles = "aeiouy"
+
+# On accueillit notre participant
+print("################################################")
+print("# BIENVENUE A DES CHIFFRES ET DES LETTRES, WOO #")
+print("################################################")
+print("#                                              #")
+print("#                       #####                  #")
+print("#                       #    #                 #")
+print("#                 #     #     #                #")
+print("#                       #     #                #")
+print("#                 #     #     #                #")
+print("#                       #    #                 #")
+print("#                       #####                  #")
+print("#                                              #")
+print("################################################")
+print
 
 # tirer 9 lettres
 tirage = []
 i = 0
 nombre_consonnes = 0
 while i < 9:
-    choix = raw_input("Voyelle ou Consonne?(V ou C): ")
+    choix = raw_input("Lettre " + str(i+1) + " de 9: Voyelle ou Consonne? (V ou C): ")
 
     #Si on choisit une consonne et qu'on peut encore
     if choix.strip().lower() == "c" and nombre_consonnes < 7:
@@ -32,9 +53,10 @@ while i < 9:
         
     
 
-print "Tirage : %s" % (" ".join(tirage).upper())
+print "\nTirage : %s" % (" ".join(tirage).upper() + "\n")
 
 #TODO FRED: test voir si ça règle les tirets
+#ferd: IT WORKS <|:D
 tirage.append("-")
 
 # on récupère le dictionnaire dans un tableau
@@ -43,31 +65,33 @@ with open("Dictionnaire.txt") as f:
     for line in f:
         dictionnaire[line.strip()] = True
 
-print ("Dico généré")
-
-# on génère les baucoup possibilités de mots avec nos 9 lettres (puis 8, etc)
+# on génère les beaucoup de possibilités de mots avec nos 9 lettres (puis 8, etc)
 found = False
 for n in xrange(9, 0, -1):
     solutions_possibles = []
 
-
     for combinaison in itertools.permutations(tirage, n):
         mot = "".join(combinaison)
-        # si le mot géné ré est dans le dico on l'ajout aux solutions
+        # si le mot généré est dans le dico, on l'ajoute aux solutions
         if dictionnaire.get(mot):
            solutions_possibles.append(mot.upper())
 
     if solutions_possibles:
-        print "Solutions possibles en %d lettres :" % n
-        solutions_possibles.sort()
-        for solution in list(set(solutions_possibles)):
+        print "\nSolutions possibles en %d lettres :" % n
+
+        for solution in sorted(list(set(solutions_possibles))):
             print solution
-        found = True
+
+        print "\n################################################"
+        print "############### YOU'RE WINNER !!! ##############"
+        print "################################################\n"
+        if os.name=='posix':
+            os.system('say "wow such winner"')
+
         break
+    
     else:
         print "Aucun mot trouvé en %d lettres" % n
 
-    if found:
-        break
-
+# GREAT SUCCESS
 
